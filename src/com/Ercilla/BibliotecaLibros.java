617193376ad70a3b751c871f0b792857df7c73ca
Conchitalb <mclb08@educastillalamancha.es>
2026-02-10 13:25:36 +0100

    Terminada implementación de la clase main

--- src/com/Ercilla/BibliotecaLibros.java
+++ src/com/Ercilla/BibliotecaLibros.java
@@ -2,6 +2,19 @@ package com.Ercilla;
 
 public class BibliotecaLibros {
     public static void main(String[] args){
+        Biblioteca biblioteca = new Biblioteca();
 
+        biblioteca.agregarLibro(new Terror("Stephen King", "It", 9));
+        biblioteca.agregarLibro(new CienciaFiccion("Asimov", "Fundación", "Trantor"));
+        biblioteca.agregarLibro(new NovelaNegra("Agatha Cristie", "Diez negritos", "Wargrave"));
+        biblioteca.agregarLibro(new NovelaRomantica("Jane Austen", "Orgullo y Prejuicio", true));
+
+        biblioteca.mostrarLibros();
+
+        System.out.println("_______ Préstamos______");
+        biblioteca.prestarLibros();
+
+        System.out.println("________ Prestar otra vez______");
+        biblioteca.prestarLibros();
     }
 }
